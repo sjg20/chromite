8dfea48e521a1efb07edc452350869fee9b4ba4b
Jack Neus <jackneus@google.com>
2022-10-25 22:28:45 +0000

    cli: cros tryjob: don't support ToT/versions >= 108
    This is a reland of commit 76a7167cf71f835de87fa3e2c17deccc407eb7c0
    
    We've added support for retrying paygen to `cros try`, which was the
    last blocker for moving Rubik branches off of `cros tryjob`.
    
    Will land this simultaneously with an announcement email in Jan '23.
    
    Original change's description:
    > cli: cros tryjob: don't support ToT/versions >= 108
    >
    > BUG=b:249954175,b:252809202
    > TEST=run_tests
    >
    > Change-Id: Ib624091ea9249e4c6588e812b056ad79833ddaee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3979645
    > Commit-Queue: Jack Neus <jackneus@google.com>
    > Tested-by: Jack Neus <jackneus@google.com>
    > Reviewed-by: Benjamin Shai <bshai@google.com>
    > Reviewed-by: Chris Gerber <gerb@google.com>
    
    BUG=b:249954175,b:252809202
    TEST=run_tests, manual runs
    
    Change-Id: Ib03ee4b05904a6e91afd4ea1ae1fd87c855a9b12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/4128776
    Reviewed-by: Madeleine Hardt <hardtmad@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Jack Neus <jackneus@google.com>
    Commit-Queue: Jack Neus <jackneus@google.com>

--- cli/cros/cros_tryjob.py
+++ cli/cros/cros_tryjob.py
@@ -420,6 +420,28 @@ def VerifyOptions(options, site_config):
             if not cros_build_lib.BooleanPrompt(prompt=prompt, default=False):
                 cros_build_lib.Die("No confirmation.")
 
+    unsupported_tryjobs = []
+    for build_config in options.build_configs:
+        for suffix in ("-release", "-payloads", "-full"):
+            if suffix in build_config:
+                unsupported_tryjobs.append(build_config)
+                break
+    if unsupported_tryjobs:
+        unsupported_branch = False
+        if on_branch:
+            tokens = options.branch.split("-")
+            if tokens[0] in ("release", "stabilize", "firmware"):
+                version = tokens[-1].split(".")
+                major = int(version[0])
+                if major >= 15183:
+                    unsupported_branch = True
+        if not on_branch or unsupported_branch:
+            cros_build_lib.Die(
+                "`cros tryjob` is unsupported for %s on milestones >= 108, "
+                "please use `cros try`.",
+                ",".join(unsupported_tryjobs),
+            )
+
     # Ensure that production configs are only run with --production.
     if not (on_branch or options.production or options.where == CBUILDBOT):
         # We can't know if branched configs are tryjob safe.
