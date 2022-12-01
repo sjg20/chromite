52efbfdc210624f1895d5994149f53c3a4457f29
Moises Veleta <moises.veleta@intel.com>
2022-12-01 14:40:51 -0800

    cros: usergroup_baseline: Add new group mei-access
    BUG=b:262025247
    TEST=none
    
    Cq-Depend: chromium:4073050
    Change-Id: I3842c5f361d1e8844677c88b47e60ec6f3ddc339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/4072685
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Tested-by: Moises Veleta <moises.veleta@intel.com>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Moises Veleta <moises.veleta@intel.com>

--- cros/test/usergroup_baseline.py
+++ cros/test/usergroup_baseline.py
@@ -439,6 +439,11 @@ GROUP_BASELINE = dict(
             gid=299,
             users={"crosvm", "shadercached"},
         ),
+        GroupEntry(
+            group="mei-access",
+            gid=427,
+            users={"healthd_psr"}
+        ),
     )
 )
 
