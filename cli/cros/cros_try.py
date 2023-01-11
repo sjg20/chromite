1d5b989bbda18c78d6919badb54b18c1891b56d0
Jack Neus <jackneus@google.com>
2023-01-11 00:46:00 +0000

    cli: bump `cros try` cipd pin
    BUG=b:264698410
    TEST=manual run
    
    Change-Id: Icc7033fa5dafca10249b3e090eca8858ef8c04af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/4153077
    Reviewed-by: Madeleine Hardt <hardtmad@google.com>
    Auto-Submit: Jack Neus <jackneus@google.com>
    Commit-Queue: Jack Neus <jackneus@google.com>
    Tested-by: Jack Neus <jackneus@google.com>
    Commit-Queue: Madeleine Hardt <hardtmad@google.com>

--- cli/cros/cros_try.py
+++ cli/cros/cros_try.py
@@ -20,8 +20,7 @@ from chromite.lib import cros_build_lib
 
 
 CIPD_TRY_PACKAGE = "chromiumos/infra/try/linux-amd64"
-PINNED_TRY_VERSION = "EoYu7bZNDmXyq6cKNKY2Mk96rVoNZXx-OSuJ9cMtf7gC"
-
+PINNED_TRY_VERSION = "CEXpLEbLhmQGZu5_1nkPX19OeRXnrPO3a7m4qzugYAcC"
 
 @command.command_decorator("try")
 class TryCommand(command.CliCommand):
