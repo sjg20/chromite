84eab9620c2931c225d026293ec84007b7bf10d4
Sergey Frolov <sfrolov@google.com>
2023-02-01 18:01:39 -0700

    lib: add unit tests for firmware_lib
    Test ssh and servo args for Flash.
    Test ssh args for read.
    
    BUG=b:259608929
    TEST=CQ
    
    Cq-Depend: chromium:4201458
    Change-Id: Ie6be0217dac03f99f9f835ded04701197d62bce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/4216253
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: Sergey Frolov <sfrolov@google.com>
    Reviewed-by: Sergey Frolov <sfrolov@google.com>
    Auto-Submit: Sergey Frolov <sfrolov@google.com>
    Commit-Queue: Sergey Frolov <sfrolov@google.com>

--- lib/firmware/firmware_lib_unittest.py
+++ lib/firmware/firmware_lib_unittest.py
@@ -1,17 +1,24 @@
 # Copyright 2021 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Tests for the firmware_lib module."""
 
+import os
+import tempfile
 from unittest import mock
 
 from chromite.lib import build_target_lib
 from chromite.lib import cros_build_lib
 from chromite.lib import cros_test_lib
 from chromite.lib import osutils
+from chromite.lib.firmware import dut
 from chromite.lib.firmware import firmware_config
 from chromite.lib.firmware import firmware_lib
+from chromite.lib.firmware import servo_lib
+
+
+# pylint: disable=unused-argument
+# pylint: disable=protected-access
 
 
 class CleanTest(cros_test_lib.RunCommandTestCase):
@@ -26,7 +33,7 @@ class CleanTest(cros_test_lib.RunCommandTestCase):
         ]
 
     def test_clean(self):
-        """Sanity check for the clean command (ideal case)."""
+        """Smoke check for the clean command (ideal case)."""
         fw_config = mock.MagicMock(
             build_workon_packages=None, build_packages=("pkg3", "pkg4")
         )
@@ -60,3 +67,231 @@ class CleanTest(cros_test_lib.RunCommandTestCase):
         self.PatchObject(cros_build_lib, "run", side_effect=se)
         with self.assertRaisesRegex(firmware_lib.CleanError, "qfile"):
             firmware_lib.clean(build_target_lib.BuildTarget("schrodinger"))
+
+
+class FlashTest(cros_test_lib.RunCommandTestCase):
+    """Tests for flashing firmware."""
+
+    def setUp(self):
+        self.build_target = build_target_lib.BuildTarget("amd64-generic")
+        self.flashrom = False
+        self.verbose = False
+        self.dryrun = False
+        self.flash_contents = None
+        self.ssh_ip = "1.2.3.4"
+        self.ssh_port = "22"
+        self.servo_port = "9999"
+        self.passthrough_args = None
+
+    def test_servo_args(self):
+        """Sanity check for the clean command (ideal case)."""
+
+        def run_side_effect(*args, **kwargs):
+            pass
+
+        run_mock = self.PatchObject(
+            cros_build_lib, "run", side_effect=run_side_effect
+        )
+
+        def get_servo_side_effect() -> servo_lib.Servo:
+            return servo_lib.Servo("servo_v4p1_with_ccd", "123456ab")
+
+        get_servo_mock = self.PatchObject(
+            dut.DutControl, "get_servo", side_effect=get_servo_side_effect
+        )
+
+        with tempfile.NamedTemporaryFile() as image_file:
+            firmware_lib._deploy_servo(
+                self.build_target,
+                image_file.name,
+                self.flashrom,
+                self.verbose,
+                self.servo_port,
+                self.dryrun,
+                self.flash_contents,
+                self.passthrough_args,
+            )
+        run_mock.assert_has_calls(
+            [
+                mock.call(
+                    ["dut-control", f"--port={self.servo_port}", mock.ANY],
+                    print_cmd=False,
+                    dryrun=False,
+                ),
+                mock.call(
+                    [
+                        "sudo",
+                        "--",
+                        "futility",
+                        "update",
+                        "-p",
+                        "raiden_debug_spi:target=AP,serial=123456ab",
+                        "-i",
+                        mock.ANY,
+                        "--force",
+                        "--wp=0",
+                        "--fast",
+                    ],
+                    print_cmd=False,
+                    dryrun=False,
+                ),
+            ]
+        )
+        get_servo_mock.assert_any_call()
+
+    def test_ssh_args(self):
+        """Sanity check for the clean command (ideal case)."""
+
+        def run_side_effect(*args, **kwargs):
+            pass
+
+        run_mock = self.PatchObject(
+            cros_build_lib, "run", side_effect=run_side_effect
+        )
+
+        with tempfile.NamedTemporaryFile() as image_file:
+            firmware_lib._deploy_ssh(
+                self.build_target,
+                image_file.name,
+                self.flashrom,
+                self.verbose,
+                self.ssh_ip,
+                self.ssh_port,
+                self.dryrun,
+                self.passthrough_args,
+            )
+            ssh_keys_expected = ["-i", mock.ANY]
+            if os.path.exists(firmware_lib._ssh_partner_id_filename):
+                ssh_keys_expected += ["-i", mock.ANY]
+            run_mock.assert_has_calls(
+                [
+                    mock.call(
+                        [
+                            "scp",
+                            "-i",
+                            mock.ANY,
+                            "-i",
+                            mock.ANY,
+                            "-P",
+                            self.ssh_port,
+                            "-o",
+                            "UserKnownHostsFile=/dev/null",
+                            "-o",
+                            "StrictHostKeyChecking=no",
+                            "-o",
+                            "CheckHostIP=no",
+                            mock.ANY,
+                            f"root@{self.ssh_ip}:/tmp",
+                        ],
+                        print_cmd=False,
+                        check=True,
+                        dryrun=False,
+                    ),
+                    mock.call(
+                        [
+                            "ssh",
+                            f"root@{self.ssh_ip}",
+                            *ssh_keys_expected,
+                            "-p",
+                            self.ssh_port,
+                            "-o",
+                            "UserKnownHostsFile=/dev/null",
+                            "-o",
+                            "StrictHostKeyChecking=no",
+                            "-o",
+                            "CheckHostIP=no",
+                            "futility",
+                            "update",
+                            "-p",
+                            "host",
+                            "-i",
+                            mock.ANY,
+                            "&& reboot",
+                        ],
+                        print_cmd=False,
+                        check=True,
+                        dryrun=False,
+                    ),
+                ]
+            )
+
+
+class ReadTest(cros_test_lib.RunCommandTestCase):
+    """Tests for reading firmware."""
+
+    def setUp(self):
+        self.verbose = False
+        self.dryrun = False
+        self.ssh_ip = "1.2.3.4"
+        self.ssh_port = "22"
+        self.region_to_read = ""
+
+    def test_ssh_args(self):
+        """Sanity check for the clean command (ideal case)."""
+
+        def run_side_effect(*args, **kwargs):
+            pass
+
+        run_mock = self.PatchObject(
+            cros_build_lib, "run", side_effect=run_side_effect
+        )
+
+        with tempfile.NamedTemporaryFile() as image_file:
+            firmware_lib.ssh_read(
+                image_file.name,
+                self.verbose,
+                self.ssh_ip,
+                self.ssh_port,
+                self.dryrun,
+                self.region_to_read,
+            )
+            ssh_keys_expected = ["-i", mock.ANY]
+            if os.path.exists(firmware_lib._ssh_partner_id_filename):
+                ssh_keys_expected += ["-i", mock.ANY]
+
+            run_mock.assert_has_calls(
+                [
+                    mock.call(
+                        [
+                            "ssh",
+                            f"root@{self.ssh_ip}",
+                            *ssh_keys_expected,
+                            "-p",
+                            self.ssh_port,
+                            "-o",
+                            "UserKnownHostsFile=/dev/null",
+                            "-o",
+                            "StrictHostKeyChecking=no",
+                            "-o",
+                            "CheckHostIP=no",
+                            "flashrom",
+                            "-p",
+                            "host",
+                            "-r",
+                            image_file.name,
+                        ],
+                        print_cmd=False,
+                        check=True,
+                        dryrun=False,
+                    ),
+                    mock.call(
+                        [
+                            "scp",
+                            *ssh_keys_expected,
+                            "-P",
+                            self.ssh_port,
+                            "-o",
+                            "UserKnownHostsFile=/dev/null",
+                            "-o",
+                            "StrictHostKeyChecking=no",
+                            "-o",
+                            "CheckHostIP=no",
+                            mock.ANY,
+                            image_file.name,
+                        ],
+                        print_cmd=False,
+                        check=True,
+                        dryrun=False,
+                    ),
+                ]
+            )
