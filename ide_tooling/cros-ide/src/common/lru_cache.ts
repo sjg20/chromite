9dd14b8ba830aa2f041dfb4d39c85b74919ce453
Keigo Oka <oka@google.com>
2023-04-17 14:13:52 +0000

    cros-ide: Add generic LRU cache implementation
    The cache will be used to introduce caching in clients used in Gerrit
    feature.
    
    BUG=b:270101085
    TEST=npm run unit-test
    
    Change-Id: I9b5bfbf6266bd1157c7e2f4d0bd67354555925e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/4431784
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>

---
+++ ide_tooling/cros-ide/src/common/lru_cache.ts
@@ -0,0 +1,67 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Generic LRU cache implementation whose methods have amortized time complexity
+ * of O(log n).
+ */
+export class LruCache<K, V> {
+  /**
+   * Creates an LRU cache. The number n denotes the guaranteed capacity of the
+   * cache. The number of cached entries doesn't exceed 2n at any point of time.
+   */
+  constructor(private readonly n: number) {
+    if (n <= 0) {
+      throw new Error(`LRU cache capacity must be positive but was ${n}`);
+    }
+  }
+
+  private nextTicketNumber = 0;
+
+  private readonly cache = new Map<K, {ticket: number; value: V}>();
+
+  /**
+   * Sets the key value pair with
+   * - worst time complexity O(n log n), and
+   * - amortized time complexity O(log n).
+   */
+  set(key: K, value: V): void {
+    this.cache.set(key, {ticket: this.nextTicketNumber++, value});
+    this.maybeShrink();
+  }
+
+  /**
+   * Gets the cached value or undefined if it doesn't exist.
+   */
+  get(key: K): V | undefined {
+    const cached = this.cache.get(key);
+    if (!cached) return undefined;
+
+    this.cache.set(key, {ticket: this.nextTicketNumber++, value: cached.value});
+    return cached.value;
+  }
+
+  /**
+   * Evicts the key from the cache.
+   */
+  evict(key: K): void {
+    this.cache.delete(key);
+  }
+
+  // Shrinks the cache if the size >= 2n.
+  private maybeShrink() {
+    if (this.cache.size < 2 * this.n) return;
+
+    // Run O(n log n) operation to half the data size.
+    const lru: {ticket: number; key: K}[] = [];
+    for (const [key, {ticket}] of this.cache.entries()) {
+      lru.push({ticket, key});
+    }
+    lru.sort((x, y) => -(x.ticket - y.ticket)); // reverse order
+
+    for (const {key} of lru.slice(this.n)) {
+      this.cache.delete(key);
+    }
+  }
+}
---
+++ ide_tooling/cros-ide/src/test/unit/common/lru_cache.test.ts
@@ -0,0 +1,73 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as util from 'util';
+import {LruCache} from '../../../common/lru_cache';
+
+describe('LRU cache', () => {
+  it('works correctly', () => {
+    const n = 3;
+    const cache = new LruCache<string, number>(n);
+
+    expect(cache.get('a')).toBeUndefined();
+
+    cache.set('a', 1);
+    cache.set('b', 2);
+    cache.set('c', 3);
+    cache.set('b', 4);
+
+    expect(cache.get('a')).toEqual(1);
+    expect(cache.get('b')).toEqual(4);
+    expect(cache.get('c')).toEqual(3);
+
+    cache.evict('a');
+
+    expect(cache.get('a')).toBeUndefined();
+
+    // Test the cache doesn't hold more than 2n entries.
+    cache.set('a', 1);
+    cache.set('b', 2); // deleted
+    cache.set('c', 3); // don't care
+    cache.set('d', 4); // don't care
+    cache.set('e', 5); // don't care
+    cache.set('f', 6); // kept
+    cache.set('a', 7); // kept
+    cache.set('g', 8); // kept
+
+    expect(cache.get('b')).toBeUndefined();
+    expect(cache.get('f')).toEqual(6);
+    expect(cache.get('a')).toEqual(7);
+    expect(cache.get('g')).toEqual(8);
+
+    // Test `get` operations keep the entry away from being evicted.
+    cache.set('a', 1);
+    cache.set('b', 2);
+    cache.set('c', 3);
+    cache.get('a');
+    cache.set('d', 4);
+    cache.set('e', 5);
+    cache.get('a');
+    cache.set('f', 6);
+    cache.set('g', 8);
+
+    expect(cache.get('a')).toEqual(1);
+  });
+
+  it('works efficiently', async () => {
+    const n = 1000;
+    const cache = new LruCache(n);
+
+    const m = 100_000;
+
+    // m log n ~ 1_000_000.
+    for (let i = 0; i < m; i++) {
+      cache.set(i, i);
+    }
+    expect(cache.get(0)).toBeUndefined();
+    expect(cache.get(m - 1)).toEqual(m - 1);
+
+    // Ensure timeout works.
+    await util.promisify(setImmediate)();
+  }, 1000);
+});
